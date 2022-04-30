8d58376f1c5b7b4d36ad90b5946de1ae9f6db831
Your Name <vlacicnikoal00@gmail.com>
2022-04-30 19:40:25 +0200

    Added a clear button

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.graphics import Line, Color, Ellipse
 import random 
+from kivy.uix.button import Button
+
 
 Window.clearcolor = (1,1,1,1)
 class PaintWindow(Widget):
@@ -17,9 +19,21 @@ class PaintWindow(Widget):
 		self.canvas.add(touch.ud['line'])
 	def on_touch_move(self, touch):
 		touch.ud['line'].points  += [touch.x, touch.y]
-class PaintApp(App):
-	def build(self):
-		return PaintWindow()
 		
+
+
+class PaintApp(App):
+	def clear_canvas(self, obj):
+		self.painter.canvas.clear()	
+	def build(self):
+		rootWindow = Widget()
+		self.painter = PaintWindow()
+		clearBtn = Button(text = 'Clear')
+		clearBtn.bind(on_release=self.clear_canvas)
+		rootWindow.add_widget(self.painter)
+		rootWindow.add_widget(clearBtn)
+		return rootWindow
+		
+	
 PaintApp().run()
 
