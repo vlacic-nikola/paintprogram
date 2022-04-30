94513ca22560affc962e7e11df59a163d0079392
Your Name <vlacicnikoal00@gmail.com>
2022-04-30 19:27:13 +0200

    Ability to draw lines added

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.graphics import Line, Color, Ellipse
+import random 
 
 Window.clearcolor = (1,1,1,1)
 class PaintWindow(Widget):
 	def on_touch_down(self, touch):
-		self.canvas.add(Color(rgb = (1,0,0,1)))
+		colorR = random.randint(0,255)
+		colorB = random.randint(0,255)
+		colorG = random.randint(0,255)
+		self.canvas.add(Color(rgb = (colorR/255.0,colorB/255.0,colorG/255.0,1)))
 		d = 30
 		self.canvas.add(Ellipse(pos=(touch.x - d/2, touch.y - d/2), size = (d,d)))
 		touch.ud['line'] = Line(points=(touch.x, touch.y))
